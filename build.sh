16942327d2b1bd48136bc48fd972edb2fcf3e86c
WakelessSloth56 <wsloth56@foxmail.com>
2023-11-02 21:09:19 +0800

    build(style): scss

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ npm install
 commit=$(git rev-parse --verify HEAD)
 branch=$(git branch --show-current)
 sed -i "s;{version};$branch@$commit;g" src/core/main.ts
-sed -i "s;{version};$branch@$commit;g" src/style/style.scss
+sed -i "s;{version};$branch@$commit;g" src/style/main.scss
 
 node_modules/.bin/rollup -c
 
 echo '{"compress":{"pure_funcs":["console.debug"]},"ecma":2017}' >temp/terser.json
 node_modules/.bin/terser --config-file temp/terser.json dist/acdoc.js >dist/acdoc.min.js
 
-# node_modules/.bin/sass --no-source-map src/style/main.scss:dist/style.css
-# node_modules/.bin/sass --no-source-map --style compressed src/style/main.scss:dist/style.min.css
+node_modules/.bin/sass --no-source-map src/style/main.scss:dist/style.css
+node_modules/.bin/sass --no-source-map --style compressed src/style/main.scss:dist/style.min.css
